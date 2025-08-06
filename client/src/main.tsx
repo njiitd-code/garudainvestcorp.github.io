8c2b3fc925ea1a972cbd6d018d8b05c268aac139
njiitd-code <nj.iitd@gmail.com>
2025-08-06 13:40:40 +0530

    Trigger deploy

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { Router } from "wouter";
-const isGitHubPages = process.env.NODE_ENV === 'production';
 
 createRoot(document.getElementById("root")!).render(
 <Router base={import.meta.env.MODE === "production" ? "/garudainvestcorp.github.io" : "/"}>
